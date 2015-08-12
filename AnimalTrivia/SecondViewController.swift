2ca1e887e2fe409a397d665e0460b1499213f4eb
H-pack <hasaani.p@gmail.com>
2015-08-11 22:54:21 -0400

    initial commit

--- AnimalTrivia/SecondViewController.swift
+++ AnimalTrivia/SecondViewController.swift
@@ -1,81 +1,80 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  AnimalTrivia
 //
-//  Created by Hasaani Pack on 7/30/15.
+//  Created by Hasaani Pack on 8/4/15.
 //  Copyright (c) 2015 bitfountain. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondViewController: UIViewController {
+    
+    
+    
+    
     @IBOutlet weak var aLabel: UILabel!
-
-    @IBOutlet weak var bLabel: UILabel!
     
-    @IBOutlet weak var cLabel: UILabel!
-    
-   
     @IBOutlet weak var aButton: UIButton!
-    
-    @IBOutlet weak var bButton: UIButton!
-    
-    @IBOutlet weak var cButton: UIButton!
-    
     
     @IBOutlet weak var incorrectAImageView: UIImageView!
     
-    @IBOutlet weak var incorrectBImageView: UIImageView!
+   
+    @IBOutlet weak var bLabel: UILabel!
     
-    @IBOutlet weak var correctCImageView: UIImageView!
+    @IBOutlet weak var bButton: UIButton!
+    
+    @IBOutlet weak var correctBImageView: UIImageView!
     
     
+    @IBOutlet weak var cLabel: UILabel!
     
+    @IBOutlet weak var cButton: UIButton!
+    
+    @IBOutlet weak var incorrectCImageView: UIImageView!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
     @IBAction func aButtonPressed(sender: UIButton) {
-        incorrectAImageView.hidden = false
         aButton.hidden = true
+        incorrectAImageView.hidden = false
         
         bButton.enabled = false
         cButton.enabled = false
-        
         aLabel.textColor = UIColor(red: 0.98, green: 0.39, blue: 0.4, alpha: 1.0)
+        
     }
+
     
     @IBAction func bButtonPressed(sender: UIButton) {
-        incorrectBImageView.hidden = false
         bButton.hidden = true
+        correctBImageView.hidden = false
         
         aButton.enabled = false
         cButton.enabled = false
-        
-        bLabel.textColor = UIColor(red: 0.98, green: 0.39, blue: 0.4, alpha: 1.0)
+        bLabel.textColor = UIColor.greenColor()
     }
     
+    
     @IBAction func cButtonPressed(sender: UIButton) {
-        correctCImageView.hidden = false
         cButton.hidden = true
+        incorrectCImageView.hidden = false
         
         bButton.enabled = false
         aButton.enabled = false
-        
-        cLabel.textColor = UIColor.greenColor()
+        cLabel.textColor = UIColor(red: 0.98, green: 0.39, blue: 0.4, alpha: 1.0)
     }
     
     
     
-    
-    
-
 }
-
---
+++ AnimalTrivia/ThirdViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ThirdViewController.swift
+//  AnimalTrivia
+//
+//  Created by Hasaani Pack on 8/4/15.
+//  Copyright (c) 2015 bitfountain. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    @IBOutlet weak var aLabel: UILabel!
+    
+    @IBOutlet weak var aButton: UIButton!
+    
+    
+    @IBOutlet weak var bLabel: UILabel!
+    
+    @IBOutlet weak var bButton: UIButton!
+    
+    
+    @IBOutlet weak var cLabel: UILabel!
+    
+    @IBOutlet weak var cButton: UIButton!
+    
+    
+    @IBOutlet weak var startOverButton: UIButton!
+    
+    let myRedColor = UIColor(red: 0.98, green: 0.39, blue: 0.4, alpha: 1.0)
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        startOverButton.layer.cornerRadius = 7.0
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func aButtonPressed(sender: UIButton) {
+        
+        let incorrectAnswerImage = UIImage(named: "incorrectAnswer")
+        aButton.setImage(incorrectAnswerImage, forState: UIControlState.Normal)
+        disableButtons()
+        aLabel.textColor = myRedColor
+    }
+    
+    
+    
+    @IBAction func bButtonPressed(sender: UIButton) {
+        let incorrectAnswerImage = UIImage(named: "incorrectAnswer")
+        bButton.setImage(incorrectAnswerImage, forState: UIControlState.Normal)
+        disableButtons()
+        bLabel.textColor = myRedColor
+    }
+    
+    
+    @IBAction func cButtonPressed(sender: UIButton) {
+        let correctAnswerImage = UIImage(named: "correctAnswer")
+        disableButtons()
+        cLabel.textColor = UIColor.greenColor()
+    }
+    
+    func disableButtons() {
+        aButton.enabled = false
+        bButton.enabled = false
+        cButton.enabled = false
+    }
+    
+    
+    
+}
