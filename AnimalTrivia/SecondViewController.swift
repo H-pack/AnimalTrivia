d24bfacbcc237cee9e1fa4f6ed69f36bca4effe5
H-pack <hasaani.p@gmail.com>
2015-08-28 21:59:58 -0400

    used swift to change the image of the buttons in all the view controllers instead of having hidden images over the buttons in the first few view controllers.

--- AnimalTrivia/SecondViewController.swift
+++ AnimalTrivia/SecondViewController.swift
@@ -17,21 +17,19 @@ class SecondViewController: UIViewController {
     
     @IBOutlet weak var aButton: UIButton!
     
-    @IBOutlet weak var incorrectAImageView: UIImageView!
-    
    
     @IBOutlet weak var bLabel: UILabel!
     
     @IBOutlet weak var bButton: UIButton!
-    
-    @IBOutlet weak var correctBImageView: UIImageView!
-    
+   
     
     @IBOutlet weak var cLabel: UILabel!
-    
+  
     @IBOutlet weak var cButton: UIButton!
     
-    @IBOutlet weak var incorrectCImageView: UIImageView!
+    let myRedColor = UIColor(red: 0.98, green: 0.39, blue: 0.4, alpha: 1.0)
+    let correctAnswerImage = UIImage(named: "correctAnswer")
+    let incorrectAnswerImage = UIImage(named: "incorrectAnswer")
     
 
     override func viewDidLoad() {
@@ -44,37 +42,31 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     @IBAction func aButtonPressed(sender: UIButton) {
-        aButton.hidden = true
-        incorrectAImageView.hidden = false
-        
-        bButton.enabled = false
-        cButton.enabled = false
-        aLabel.textColor = UIColor(red: 0.98, green: 0.39, blue: 0.4, alpha: 1.0)
-        
+        aButton.setImage(incorrectAnswerImage, forState: UIControlState.Normal)
+        disableButtons()
+        aLabel.textColor = myRedColor
+    }
+    
+    @IBAction func bButtonPressed(sender: UIButton) {
+        bButton.setImage(correctAnswerImage, forState: UIControlState.Normal)
+        disableButtons()
+        bLabel.textColor = UIColor.greenColor()
     }
 
     
-    @IBAction func bButtonPressed(sender: UIButton) {
-        bButton.hidden = true
-        correctBImageView.hidden = false
-        
-        aButton.enabled = false
-        cButton.enabled = false
-        bLabel.textColor = UIColor.greenColor()
-    }
-    
-    
     @IBAction func cButtonPressed(sender: UIButton) {
-        cButton.hidden = true
-        incorrectCImageView.hidden = false
-        
-        bButton.enabled = false
-        aButton.enabled = false
-        cLabel.textColor = UIColor(red: 0.98, green: 0.39, blue: 0.4, alpha: 1.0)
+        cButton.setImage(incorrectAnswerImage, forState: UIControlState.Normal)
+        disableButtons()
+        cLabel.textColor = myRedColor
     }
     
-    
+    func disableButtons() {
+        aButton.enabled = false
+        bButton.enabled = false
+        cButton.enabled = false
+    }
+
     
 }
--- SixthViewController.swift
+++ SixthViewController.swift
@@ -1,34 +1,36 @@
 //
-//  ViewController.swift
+//  SixthViewController.swift
 //  AnimalTrivia
 //
-//  Created by Hasaani Pack on 7/30/15.
-//  Copyright (c) 2015 bitfountain. All rights reserved.
+//  Created by Hasaani Pack on 8/28/15.
+//  Copyright © 2015 bitfountain. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var aLabel: UILabel!
+class SixthViewController: UIViewController {
 
-    @IBOutlet weak var bLabel: UILabel!
-    
-    @IBOutlet weak var cLabel: UILabel!
-    
-   
+    let myRedColor = UIColor(red: 0.98, green: 0.39, blue: 0.4, alpha: 1.0)
+    let correctAnswerImage = UIImage(named: "correctAnswer")
+    let incorrectAnswerImage = UIImage(named: "incorrectAnswer")
+
     @IBOutlet weak var aButton: UIButton!
     
     @IBOutlet weak var bButton: UIButton!
     
     @IBOutlet weak var cButton: UIButton!
     
-    let myRedColor = UIColor(red: 0.98, green: 0.39, blue: 0.4, alpha: 1.0)
-    let correctAnswerImage = UIImage(named: "correctAnswer")
-    let incorrectAnswerImage = UIImage(named: "incorrectAnswer")
+    @IBOutlet weak var aLabel: UILabel!
     
+    @IBOutlet weak var bLabel: UILabel!
+    
+    @IBOutlet weak var cLabel: UILabel!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,17 +44,16 @@ class ViewController: UIViewController {
         aLabel.textColor = myRedColor
     }
     
-    
     @IBAction func bButtonPressed(sender: UIButton) {
-        bButton.setImage(incorrectAnswerImage, forState: UIControlState.Normal)
+        bButton.setImage(correctAnswerImage, forState: UIControlState.Normal)
         disableButtons()
-        aLabel.textColor = myRedColor
+        bLabel.textColor = UIColor.greenColor()
     }
     
     @IBAction func cButtonPressed(sender: UIButton) {
-        cButton.setImage(correctAnswerImage, forState: UIControlState.Normal)
+        cButton.setImage(incorrectAnswerImage, forState: UIControlState.Normal)
         disableButtons()
-        cLabel.textColor = UIColor.greenColor()
+        cLabel.textColor = myRedColors
     }
     
     func disableButtons() {
@@ -61,5 +62,5 @@ class ViewController: UIViewController {
         cButton.enabled = false
     }
 
-}
 
+}
