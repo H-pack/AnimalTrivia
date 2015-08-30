ab0a116a35a623c1e6f6b9bdb88c5ee809c3dfe5
H-pack <hasaani.p@gmail.com>
2015-08-30 15:53:15 -0400

    Almost done! All thats left to do is make sure each view is uniform and connect the last view controller to a .swift file.

---
+++ AnimalTrivia/SeventhViewController.swift
@@ -0,0 +1,77 @@
+//
+//  SeventhViewController.swift
+//  AnimalTrivia
+//
+//  Created by Hasaani Pack on 8/30/15.
+//  Copyright © 2015 bitfountain. All rights reserved.
+//
+
+import UIKit
+
+class SeventhViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mantidImageView: UIImageView!
+    
+    @IBOutlet weak var aButton: UIButton!
+    
+    @IBOutlet weak var bButton: UIButton!
+    
+    @IBOutlet weak var cButton: UIButton!
+    
+    
+    @IBOutlet weak var aLabel: UILabel!
+    
+    @IBOutlet weak var bLabel: UILabel!
+    
+    @IBOutlet weak var cLabel: UILabel!
+    
+    let myRedColor = UIColor(red: 0.98, green: 0.39, blue: 0.4, alpha: 1.0)
+    let correctAnswerImage = UIImage(named: "correctAnswer")
+    let incorrectAnswerImage = UIImage(named: "incorrectAnswer")
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        mantidImageView.layer.borderWidth = 1
+        mantidImageView.layer.masksToBounds = false
+        mantidImageView.layer.borderColor = UIColor.blackColor().CGColor
+        mantidImageView.layer.cornerRadius = mantidImageView.frame.height/2
+        mantidImageView.clipsToBounds = true
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func aButtonPressed(sender: UIButton) {
+        aButton.setImage(incorrectAnswerImage, forState: UIControlState.Normal)
+        disableButtons()
+        aLabel.textColor = myRedColor
+    }
+    
+    
+    @IBAction func bButtonPressed(sender: UIButton) {
+        bButton.setImage(correctAnswerImage, forState: UIControlState.Normal)
+        disableButtons()
+        bLabel.textColor = UIColor.greenColor()
+    }
+    
+    
+    @IBAction func cButtonPressed(sender: UIButton) {
+        cButton.setImage(incorrectAnswerImage, forState: UIControlState.Normal)
+        disableButtons()
+        cLabel.textColor = myRedColor
+    }
+    
+    func disableButtons() {
+        aButton.enabled = false
+        bButton.enabled = false
+        cButton.enabled = false
+    }
+
+
+}
